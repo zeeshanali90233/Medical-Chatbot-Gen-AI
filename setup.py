9233286a243381cfea95ad49896833864d1f46a7
Zeeshan ali <zeeshanali90233@gmail.com>
2024-08-14 17:34:00 +0500

    Fiels

---
+++ setup.py
@@ -0,0 +1,14 @@
+#  
+from setuptools import setup ,find_packages
+
+
+setup( 
+	name='chatbot_package', 
+	version='0.1', 
+	description='A GEN AI Medical Chatbot', 
+	author='Zeeshan Ali', 
+	author_email='zeeshanali90233@gmail.com', 
+	packages=find_packages(), 
+    install_requires=[]
+) 
+
